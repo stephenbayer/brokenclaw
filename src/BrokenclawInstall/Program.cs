2cbc303df5b9c2f729b4f27d7da3bd5a087b6882
Stephen A. Bayer <sabayer@gmail.com>
2019-04-13 19:56:20 -0700

    Setup project

---
+++ src/BrokenclawInstall/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WixSharp;
+
+// DON'T FORGET to update NuGet package "WixSharp".
+// NuGet console: Update-Package WixSharp
+// NuGet Manager UI: updates tab
+
+namespace BrokenclawInstall
+{
+    class Program
+    {
+        static void Main()
+        {
+            var project = new Project("BrokenclawApp",
+                              new Dir(@"%ProgramFiles%\StephenBayer\Brokenclaw",
+                                  new File(@"Files\Docs\readme.txt")));
+
+            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+            //project.SourceBaseDir = "<input dir path>";
+            project.OutDir = "build/msi";
+
+            project.BuildMsi();
+        }
+    }
+}
